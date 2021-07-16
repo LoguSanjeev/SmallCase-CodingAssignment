6bd8df839f6a630d14fd9ce5b2f04574a0074c54
Sanjeev, Logu <logu.sanjeev@hrblock.com>
2021-07-16 17:14:17 +0530

    Added Comment lines for Test Cases

--- src/Tests_UI/Tests.java
+++ src/Tests_UI/Tests.java
@@ -30,6 +30,7 @@ public class Tests {
 	@Test(priority = 1, description = "Finding the total price of the Item after increasing the Quantity in Flipkart")
 	public void flipkartPrintThePriceOfTheProduct() {
 
+//Launching FlipKart and searching product
 		Commons common = new Commons(driver);
 		common.navigateTo("https://www.flipkart.com/");
 		common.clickElement(FlipkartMainPage.closeLoginPopupButton);
@@ -38,6 +39,9 @@ public class Tests {
 		common.clickElementFromList(FlipkartMainPage.productList, 0);
 		common.switchToNextWindow();
 
+//Printing the Price from product page and Cart(after increasing the quantity)
+		System.out.println("Test Case 1 \n");
+		System.out.println("***** FlipKart Price*****");
 		System.out.println("Price of \"" + productName + "\" at Product Page is: "
 				+ common.getTextFromElement(FlipkartProductPage.productPriceText).substring(1));
 
@@ -52,6 +56,7 @@ public class Tests {
 	@Test(priority = 2, description = "Comparing the price of the product between Amazon and Flipkart")
 	public void priceComparisionBetweenFlipKartAndAmazon() {
 
+//Launching Flipkart
 		Commons common = new Commons(driver);
 		common.navigateTo("https://www.flipkart.com/");
 		parentWindow = driver.getWindowHandle();
@@ -61,33 +66,39 @@ public class Tests {
 		common.clickElementFromList(FlipkartMainPage.productList, 0);
 		common.switchToNextWindow();
 
+//Printing the price of the product from product page and Cart
+		System.out.println("\n\n\nTest Case 2 \n");
+		System.out.println("*****Flipkart Price****");
 		System.out.println("Price of \"" + productName + "\" at Product Page is: "
 				+ common.getTextFromElement(FlipkartProductPage.productPriceText).substring(1));
 
 		common.clickElement(FlipkartProductPage.addToCartButton);
 		String FlipkartPrice = common.getTextFromElement(FlipkartCartPage.totalPriceText).substring(1);
-		System.out.println("Total price of the Product \"" + productName + "\" at cart is: " + FlipkartPrice);
+		System.out.println("Total price of the Product \"" + productName + "\" at cart is: " + FlipkartPrice+"\n");
 
 		driver.close();
 		common.switchToParentWindow(parentWindow);
 
-		// ---------
 
+//Launching Amazon
 		common.navigateTo("https://www.amazon.in/");
 		common.setValueToTextBox(AmazonMainPage.searchBox, productName);
 		common.clickElement(AmazonMainPage.searchButton);
 		common.clickElementFromList(AmazonMainPage.productList, 0);
 
+//Printing the price from product age and cart
 		common.switchToNextWindow();
-		String s=common.getTextFromElement(AmazonProductPage.productPriceText);
+		System.out.println("*****Amazon Price****");
 		System.out.println("Amazon Price of \"" + productName + "\" is: "
-				+ common.getTextFromElement(AmazonProductPage.productPriceText));
+				+ common.getTextFromElement(AmazonProductPage.productPriceText).substring(1));
 
 		common.clickElement(AmazonProductPage.addToCartButton);
 		String AmazonPrice = common.getTextFromElement(AmazonCartPage.totalPriceText).substring(1);
-		System.out.println("Amazon Price of \"" + productName + "\" after adding to cart is: " + AmazonPrice);
+		System.out.println("Amazon Price of \"" + productName + "\" after adding to cart is: " + AmazonPrice+"\n");
 
-		System.out.print(common.findCheaperRate(FlipkartPrice, AmazonPrice));
+//Comparing Amazon and Flipkart and Printing the cheaper price
+		System.out.println("=====Cheaper Price====");
+		System.out.println("====="+common.findCheaperRate(FlipkartPrice, AmazonPrice)+"=====");
 	}
 
 	@AfterMethod
