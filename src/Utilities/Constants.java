1383a30486c1b655b84e741b9980f6e03edc48f1
Sanjeev, Logu <logusanjeev777@gmail.com>
2021-07-17 21:26:50 +0530

    Final Code- Added Constants Class and maven dependencies

---
+++ src/Utilities/Constants.java
@@ -0,0 +1,8 @@
+package Utilities;
+
+public class Constants {
+public static final String AMAZON_URL="https://www.amazon.in/";
+public static final String FLIPKART_URL="https://www.flipkart.com/";
+
+
+}
